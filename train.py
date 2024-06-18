610229f82a00ddc4f2f24b5b33ad0020e9d9d308
Peter Belcak <pbelcak@nvidia.com>
2024-06-18 09:39:29 +0000

    updated train.py

--- train.py
+++ train.py
@@ -361,7 +361,6 @@ while True:
 
     # forward backward update, with optional gradient accumulation to simulate larger batch size
     # and using the GradScaler if data type is float16
-    flosses: list[list[float]] = [[] for _ in range(fmax)]
     for micro_step in range(gradient_accumulation_steps):
         if ddp:
             # in DDP training we only need to sync gradients at the last micro step.
@@ -370,42 +369,12 @@ while True:
             # looking at the source of that context manager, it just toggles this variable
             model.require_backward_grad_sync = (micro_step == gradient_accumulation_steps - 1)
         with ctx:
-            for f in range(0, fmax):
-                inX: torch.Tensor
-                if f == 0:
-                    inX = X
-                    with torch.no_grad():
-                        output = golden_model(inX, labels=Y)
-                        logits, loss = output.logits, output.loss
-                        # ensure that logits have last dim=2 model_args['vocab_size']
-                        if logits.shape[2] != model_args['vocab_size']:
-                            logits = torch.cat((logits, -100*torch.ones(logits.shape[0], logits.shape[1], model_args['vocab_size'] - logits.shape[2], dtype=ptdtype, device=device)), dim=2)
-                        # shift the logits by 1 to the right and put a one-hot encoding of the first token in the first position
-                        logits = torch.cat((torch.nn.functional.one_hot(inX[:, 0:1], num_classes=model_args['vocab_size']).to(dtype=ptdtype), logits[:, :-1]), dim=1)
-                        logits = logits[:, block_size//2:]
-                else:
-                    if len(inX.shape) == 2:
-                        # one-hot encode inX to have 3 dims
-                        inX = torch.nn.functional.one_hot(inX, num_classes=model_args['vocab_size']).to(dtype=ptdtype)
-                    
-                    outprobs = torch.softmax(logits.detach(), dim=-1)
-                    # form inX by concatenatening the first half of X with the second half of logit probs
-                    inX = torch.cat((inX[:, :block_size//2], outprobs), dim=1)
-                    logits, loss = model(inX, Y) # logits are already just for the second half of inX
-                
-                loss = loss / (gradient_accumulation_steps*(fmax-1)) # scale the loss to account for gradient accumulation
-                
-                if f == fmax-1:
-                    # immediately async prefetch next batch while model is doing the forward pass on the GPU
-                    X, Y = get_batch('train')
-                
-                if f > 0:
-                    # backward pass, with gradient scaling if training in fp16
-                    scaler.scale(loss).backward()
-                
-                # store the loss for logging
-                flosses[f].append(loss.item())
-    
+            logits, loss = model(X, Y)
+            loss = loss / gradient_accumulation_steps # scale the loss to account for gradient accumulation
+        # immediately async prefetch next batch while model is doing the forward pass on the GPU
+        X, Y = get_batch('train')
+        # backward pass, with gradient scaling if training in fp16
+        scaler.scale(loss).backward()
     # clip the gradient
     if grad_clip != 0.0:
         scaler.unscale_(optimizer)
@@ -423,13 +392,11 @@ while True:
     if iter_num % log_interval == 0 and master_process:
         # get loss as float. note: this is a CPU-GPU sync point
         # scale up to undo the division above, approximating the true total loss (exact would have been a sum)
-        agg_flosses = [sum(flosses)*(fmax-1) for flosses in flosses]
-        # build a string of flosses
-        flosses_str = ", ".join([f"{floss:.2f}" for floss in agg_flosses])
+        lossf = loss.item() * gradient_accumulation_steps
         if local_iter_num >= 5: # let the training loop settle a bit
             mfu = raw_model.estimate_mfu(batch_size * gradient_accumulation_steps, dt)
             running_mfu = mfu if running_mfu == -1.0 else 0.9*running_mfu + 0.1*mfu
-        print(f"iter {iter_num}: flosses {flosses_str}, time {dt*1000:.2f}ms, mfu {running_mfu*100:.2f}%")
+        print(f"iter {iter_num}: loss {lossf:.4f}, time {dt*1000:.2f}ms, mfu {running_mfu*100:.2f}%")
     iter_num += 1
     local_iter_num += 1
 
